70bad98594aedab7342c8aeda621c705a1822611
Nick Horton <nhorton04@gmail.com>
2021-05-12 14:49:27 -0700

    changed back

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def main():
 
     print(real_name)
 
-    col1, col2, col3, col4, col5 = st.beta_columns((1, .2, 1, .2, 2))
+    col1, col2, col3, col4, col5 = st.beta_columns((1, .1, 1, .1, 2))
 
     col1.image(uploaded_file, width=400)
 
