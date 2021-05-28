98d5646553aa89253b36aaeee8a8e60f2e337974
Nick Horton <nhorton04@gmail.com>
2021-05-28 01:11:20 -0700

    added crude models

--- train.py
+++ train.py
@@ -14,19 +14,30 @@ from models import TransformerNet, VGG16
 from utils import *
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parser for Fast-Neural-Style")
-    parser.add_argument("--dataset_path", type=str, required=True, help="path to training dataset")
-    parser.add_argument("--style_image", type=str, default="style-images/mosaic.jpg", help="path to style image")
-    parser.add_argument("--epochs", type=int, default=1, help="Number of training epochs")
-    parser.add_argument("--batch_size", type=int, default=4, help="Batch size for training")
-    parser.add_argument("--image_size", type=int, default=256, help="Size of training images")
+    parser = argparse.ArgumentParser(
+        description="Parser for Fast-Neural-Style")
+    parser.add_argument("--dataset_path", type=str,
+                        required=True, help="path to training dataset")
+    parser.add_argument("--style_image", type=str,
+                        default="style-images/mosaic.jpg", help="path to style image")
+    parser.add_argument("--epochs", type=int, default=1,
+                        help="Number of training epochs")
+    parser.add_argument("--batch_size", type=int, default=4,
+                        help="Batch size for training")
+    parser.add_argument("--image_size", type=int,
+                        default=256, help="Size of training images")
     parser.add_argument("--style_size", type=int, help="Size of style image")
-    parser.add_argument("--lambda_content", type=float, default=1e5, help="Weight for content loss")
-    parser.add_argument("--lambda_style", type=float, default=1e10, help="Weight for style loss")
+    parser.add_argument("--lambda_content", type=float,
+                        default=1e5, help="Weight for content loss")
+    parser.add_argument("--lambda_style", type=float,
+                        default=1e10, help="Weight for style loss")
     parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
-    parser.add_argument("--checkpoint_model", type=str, help="Optional path to checkpoint model")
-    parser.add_argument("--checkpoint_interval", type=int, default=1000, help="Batches between saving model")
-    parser.add_argument("--sample_interval", type=int, default=500, help="Batches between saving image samples")
+    parser.add_argument("--checkpoint_model", type=str,
+                        help="Optional path to checkpoint model")
+    parser.add_argument("--checkpoint_interval", type=int,
+                        default=1000, help="Batches between saving model")
+    parser.add_argument("--sample_interval", type=int, default=500,
+                        help="Batches between saving image samples")
     args = parser.parse_args()
 
     style_name = args.style_image.split("/")[-1].split(".")[0]
@@ -36,7 +47,8 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Create dataloader for the training data
-    train_dataset = datasets.ImageFolder(args.dataset_path, train_transform(args.image_size))
+    train_dataset = datasets.ImageFolder(
+        args.dataset_path, train_transform(args.image_size))
     dataloader = DataLoader(train_dataset, batch_size=args.batch_size)
 
     # Defines networks
@@ -70,8 +82,10 @@ if __name__ == "__main__":
         transformer.eval()
         with torch.no_grad():
             output = transformer(image_samples.to(device))
-        image_grid = denormalize(torch.cat((image_samples.cpu(), output.cpu()), 2))
-        save_image(image_grid, f"images/outputs/{style_name}-training/{batches_done}.jpg", nrow=4)
+        image_grid = denormalize(
+            torch.cat((image_samples.cpu(), output.cpu()), 2))
+        save_image(
+            image_grid, f"images/outputs/{style_name}-training/{batches_done}.jpg", nrow=4)
         transformer.train()
 
     for epoch in range(args.epochs):
@@ -87,7 +101,9 @@ if __name__ == "__main__":
             features_transformed = vgg(images_transformed)
 
             # Compute content loss as MSE between features
-            content_loss = args.lambda_content * l2_loss(features_transformed.relu2_2, features_original.relu2_2)
+            content_loss = args.lambda_content * \
+                l2_loss(features_transformed.relu2_2,
+                        features_original.relu2_2)
 
             # Compute style loss as MSE between gram matrices
             style_loss = 0
@@ -126,4 +142,5 @@ if __name__ == "__main__":
 
             if args.checkpoint_interval > 0 and batches_done % args.checkpoint_interval == 0:
                 style_name = os.path.basename(args.style_image).split(".")[0]
-                torch.save(transformer.state_dict(), f"checkpoints/{style_name}_{batches_done}.pth")
+                torch.save(transformer.state_dict(),
+                           f"checkpoints/{style_name}_{batches_done}.pth")
